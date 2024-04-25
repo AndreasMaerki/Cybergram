e9edb8ceb2a0ac7018a224e1accb24496275a691
Andreas Maerki <andreas.maerki@valora.com>
2024-04-25 11:29:59 +0200

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Andreas Maerki on 25.04.24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
