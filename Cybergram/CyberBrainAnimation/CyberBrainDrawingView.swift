0fc458189f191539ecb71ca0228d494e82590a55
Andreas Maerki <andreas.maerki@valora.com>
2025-04-17 20:14:53 +0200

    App renamed to Cybergram

--- Cybergram/CyberBrainAnimation/CyberBrainDrawingView.swift
+++ Cybergram/CyberBrainAnimation/CyberBrainDrawingView.swift
@@ -1,14 +1,12 @@
 import SwiftUI
 
-
-
 struct CyberBrainDrawingView: View {
   @State private var startDrawing = false
   private let spinnerSize: Double = 400
   private let strokeColor = Color.primaryForeground
   private let lineWidth: Double = 2
   private let animationTime: Double = 3.5
-  
+
   var body: some View {
     CyberBrainShape()
       .trim(from: startDrawing ? 0 : 1, to: 1)
